84d8387b7634366cf60f90ba9201ef919b6c14d4
Park kitae <bkt4044@naver.com>
2023-08-09 09:22:37 +0900

    후발대 solution 브랜치 병합

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,62 +1,72 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Button, Input } from "antd";
 import axios from "axios";
+import { Button, Input } from "antd";
+import { Link } from "react-router-dom";
 
 const Main: React.FC<any> = () => {
   const [data, setData] = useState([]);
+  const [temp, setTemp] = useState([]);
   const [contents, setContents] = useState<string>("");
-  const [isDeleted, setIsDeleted] = useState<boolean>(false);
 
   const fetchData = async () => {
-    // alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
-
     try {
-      const response = await axios.get(`http://localhost:4000/boards`);
+      const response = await axios.get(
+        `http://localhost:4000/boards?isDeleted=${false}`
+      );
+      const comments = await axios.get(
+        `http://localhost:4000/comments?isDeleted=${false}`
+      );
+
+      console.log("====================================");
+      console.log(comments.data);
+      console.log("====================================");
 
       setData(response.data);
+      setTemp(comments.data);
     } catch (error) {
-      alert("일시적인 오류가 발생하였습니다. 고객센터로 연락주세요.");
+      alert("데이터를 불러오는데 오류가 발생하였습니다.");
     }
-    // TODO: 데이터베이스에서 boards 리스트 가져오기
-    // TODO: 가져온 결과 배열을 data state에 set 하기
-    // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
   };
 
   useEffect(() => {
-    // TODO: 해당 useEffect는 최초 마운트시에만 동작하게 제어
     fetchData();
-  });
+  }, []);
 
   const handleBoardSubmit = async (e: any) => {
-    // alert("TODO 요구사항에 맞추어 기능을 완성해주세요.");
     e.preventDefault();
 
-    try {
-      await axios.post(`http://localhost:4000/boards`, {
-        email: localStorage.email,
-        contents,
-        isDeleted,
-      });
+    // 이메일 가져오기
+    await axios.post(`http://localhost:4000/boards`, {
+      email: localStorage.getItem("email"),
+      contents,
+      isDeleted: false,
+    });
 
-      alert(
-        "작성이 완료되었습니다. 아직 자동 새로고침이 불가하여 수동으로 갱신합니다."
-      );
-
-      window.location.reload();
-    } catch (error) {
-      alert("일시적인 오류가 발생하였습니다. 고객센터로 연락주세요.");
-    }
-
-    // TODO: 자동 새로고침 방지
-    // TODO: 이메일과 contents를 이용하여 post 요청 등록(isDeleted 기본값은 false)
-    // TODO: 네트워크 등 기타 문제인 경우, "일시적인 오류가 발생하였습니다. 고객센터로 연락주세요." alert
-    // TODO: 성공한 경우, "작성이 완료되었습니다. 아직 자동 새로고침이 불가하여 수동으로 갱신합니다." alert
-    // TODO: 처리완료 후, reload를 이용하여 새로고침
+    alert(
+      "등록이 완료되었습니다. 아직 자동 새로고침이 불가하여 수동으로 갱신합니다."
+    );
+    window.location.reload();
   };
 
   const handleInputChange = (e: any) => {
     setContents(e.target.value);
+  };
+
+  const handleDeleteButtonClick = async (itemId: number) => {
+    try {
+      await axios.patch(`http://localhost:4000/boards/${itemId}`, {
+        isDeleted: true,
+      });
+
+      alert(
+        "삭제가 완료되었습니다. 아직 자동 새로고침이 불가하여 수동으로 갱신합니다."
+      );
+
+      window.location.reload();
+    } catch (error) {
+      alert("데이터를 삭제하는 데에 오류가 발생하였습니다.");
+    }
   };
 
   return (
@@ -73,10 +83,13 @@ const Main: React.FC<any> = () => {
         {data.map((item: any, index) => (
           <ListItem key={item.id}>
             <span>
-              {index + 1}. {item.contents}
+              {index + 1}. <Link to={`/${item.id}`}>{item.contents}</Link>
             </span>
-            {/* // TODO: 로그인 한 user의 이메일과 일치하는 경우에만 삭제버튼 보이도록 제어 */}
-            <Button>삭제</Button>
+            {item.email === localStorage.getItem("email") && (
+              <Button onClick={() => handleDeleteButtonClick(item.id)}>
+                삭제
+              </Button>
+            )}
           </ListItem>
         ))}
       </ListWrapper>
